444dc9079bf123ef719d9ace322ba9779e25019c
Lenton Losper <llosperofficial@gmail.com>
2024-09-12 12:29:28 +0200

    Header&Links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
-   <h1 className='text-red-500'>App</h1>
+ <BrowserRouter>
+  <Routes>
+    <Route path='/Home' element={<Home/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    <Route path='/Signin' element={<Signin/>}></Route>
+    <Route path='/Signup' element={<Signup/>}></Route>
+    <Route path='/Profile' element={<Profile/>}></Route>
+  </Routes>
+ </BrowserRouter>
   )
 }
+   
