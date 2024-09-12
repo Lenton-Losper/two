bb2c8d597e761b4ac3be9595bf2b42b0d2668ab2
Lenton Losper <llosperofficial@gmail.com>
2024-09-12 12:31:38 +0200

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
-import {BrowserRouter,Routes,Route} from 'react-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import Profile from './pages/Profile'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
- <BrowserRouter>
-  <Routes>
-    <Route path='/Home' element={<Home/>}></Route>
-    <Route path='/about' element={<About/>}></Route>
-    <Route path='/Signin' element={<Signin/>}></Route>
-    <Route path='/Signup' element={<Signup/>}></Route>
-    <Route path='/Profile' element={<Profile/>}></Route>
-  </Routes>
- </BrowserRouter>
-  )
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-   
