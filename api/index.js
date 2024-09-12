4859b7d7bfe47ec8747516053864ff6385b05bb3
Lenton Losper <llosperofficial@gmail.com>
2024-09-12 21:58:12 +0200

    database connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();  // This must be at the top
+
 
 const app = express()
+
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log("connection successful")
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.listen(3000,()=>{
     console.log("you are listing on port 3000")
